2fc2eb0694274908d49da4b383a4a6916faf727c
Brianmuvel <brianmuvel@gmail.com>
2020-12-07 22:21:17 -0600

    clase 01

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-console.log('consola')
+console.log('Hola Koders')
+//variables
+//var
+//let
+//const
+
+//Tipos de datos
+//numeros, texto, boleanos, arreglos
+
+//las constantes siempre se nombran en mayusculas
+const NAME = "Brian"
+
+//variable que se puede cambiar su valor
+//let name_koder = "Brian"
+//convertir una string a numero  parseInt(LAVARIABLE)
+// operadores + , / , - , %
+
+//objetos
+//abstraccion de cosas 
+let name_koder = prompt('hey')
+let edad = prompt('Edad?')
+console.log(name_koder)
+
+let resultSuma = parseInt(edad) + 5
+

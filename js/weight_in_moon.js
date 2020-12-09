7e11c078a7f4b68602a1cd00efb418341f8c4366
Brianmuvel <brianmuvel@gmail.com>
2020-12-08 19:49:12 -0600

    ADD peso en la luna

--- js/weight_in_moon.js
+++ js/weight_in_moon.js
@@ -1,5 +1,4 @@
 
 let earth_weight = prompt('¿Cuál es tu peso?')
 let peso_lunar = (parseInt(earth_weight) / 9.81) * 1.622
-let resultado_earth_weight = alert('Tu peso en la luna es..'+ peso_lunar)
-
+let resultado_earth_weight = alert('Tu peso en la luna es..'+ peso_lunar + ' Kg')

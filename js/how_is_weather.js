a680afd2b9dcb3e94b667fa210ec711bc92c1df9
Brianmuvel <brianmuvel@gmail.com>
2020-12-08 21:12:34 -0600

    ADD else if al codigo

--- js/how_is_weather.js
+++ js/how_is_weather.js
@@ -1,19 +1,17 @@
 let weather = prompt('Cómo esta el clima hoy?')
 
-
 if (weather === "soleado"){
     console.log(`El clima esta soleado`)
 }
-if (weather === "lluvioso"){
+else if (weather === "lluvioso"){
     console.log(`El clima esta lluvioso`)
 }
-if (weather === "nevado"){
+else if (weather === "nevado"){
     console.log(`El clima esta nevado`)
 }
-if (weather === "nublado"){
+else if (weather === "nublado"){
     console.log(`El clima esta nublado`)
 }
-
 else{
     let otro = prompt('Entonces cómo esta tu día?')
     console.log(`El clima esta ${otro}`)

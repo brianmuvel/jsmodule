8bfd551e698fd397c2c861ad8f5854bce03ba34c
Brianmuvel <brianmuvel@gmail.com>
2020-12-08 21:40:08 -0600

    ADD switch del clima

--- js/how_is_weather.js
+++ js/how_is_weather.js
@@ -1,5 +1,5 @@
 let weather = prompt('Cómo esta el clima hoy?')
-
+/* 
 if (weather === "soleado"){
     console.log(`El clima esta soleado`)
 }
@@ -15,4 +15,22 @@ else if (weather === "nublado"){
 else{
     let otro = prompt('Entonces cómo esta tu día?')
     console.log(`El clima esta ${otro}`)
-} 
+}  */
+
+switch(weather){
+    case 'soleado':
+        console.log(`El clima esta soleado`)
+        break
+    case 'lluvioso':
+        console.log(`El clima esta lluvioso`)
+        break
+    case 'nevado':
+        console.log(`El clima esta nevado`)
+        break
+    case 'nublao':
+        console.log(`El clima esta nublado`)
+        break
+    default:
+        let otro = prompt('Entonces cómo esta tu día?')
+        console.log(`El clima esta ${otro}`)
+}

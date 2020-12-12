b0e2fa6d07dd8d21873046ae45c0830048bdf50c
Brianmuvel <brianmuvel@gmail.com>
2020-12-11 22:15:04 -0600

    cambios en ejercicios

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -13,6 +13,20 @@ function tablas (str) {
 
 }
 
+function reverse(texto) {
+    let palabra = texto.replace(/ /g, '')
+    let reversa = ''
+    for(let i = palabra.length-1; i >= 0; i--){
+        reversa += palabra[i]
+    }
+    if (reversa === palabra){
+        console.log('ESO CHINGA')
+    } else{
+        console.log('no es palidromo')
+    }
+}
+
+
 function queEs(tipo1, tipo2){
     console.log(typeof tipo1, typeof tipo2)
 }

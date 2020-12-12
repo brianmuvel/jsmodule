a28c2872d742ce3281b658f6cdc093217bdb473b
Brianmuvel <brianmuvel@gmail.com>
2020-12-11 21:48:39 -0600

    ejercicios

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -1,73 +1,18 @@
-/* 
-let tabla = 7
-let multiplicador = 1
-
-for(let i=10 ; i>=0; i--){
-    multiplicador = i
-    console.log(tabla + ' X  ' + multiplicador + ' = ' + tabla*multiplicador)
-} */
-/* 
-for (let i=1; i <=100; i++){
-    if(i%2 === 0){
-        console.log(i)
+//funciones
+function tablas (str) {
+    
+    let arreglo = str.slice()
+    
+    
+    for (let j=0; j<= str.length-1; j++){
+        let tabla = arreglo[j]
+        for (let i=1; i<= 10; i++){
+            console.log(`${tabla} X ${i}  =  ${tabla*i}`)
+        }
     }
-} */
-//let contador = 1
-/* while(contador <= 100){
-    if(contador % 2 === 0){
-        console.log(contador)
-    }
-    contador++
-} */
-/* 
-do{
-    if(contador % 2 === 0){
-        console.log(contador)
-    }
-    contador++
-}while(contador <= 100)
- */
-/* 
-let palabra = 'reverse'
-let reversa = ''
-for(let i = palabra.length; i >= 0; i--){
-    reversa = palabra[i]
-    console.log(reversa)
-} */
-
-/* 
-let palabra = 'reverse'
-let contadorr = palabra.length - 1
-let reversa = ''
-do{
-    reversa += palabra[contadorr]
-    contadorr--
-}while(contadorr >= 0)
-
-console.log(reversa)
- */
-//
-/* let counter = 1
-white(counter <= 10){
-    console.log(`7 X ${counter} = ${7*counter}`)
-    counter++
-} */
-//do white
-/* do{
-    console.log(contador)
-}while (counter<=10) */
-
-//FUNCIONES
-
-/* function myFunction () {
-    //sentencias
 
 }
 
-function myFunction (parametro1, parametro2,....) {
-    //sentencias
-
+function queEs(tipo1, tipo2){
+    console.log(typeof tipo1, typeof tipo2)
 }
- */
-
- //10 ejercicis a funciones con descripcion 

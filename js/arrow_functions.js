b1f2eb41d02265551ad6f0942d13d2a5cb0889c3
Brianmuvel <brianmuvel@gmail.com>
2020-12-14 20:33:04 -0600

    ADD inicial

---
+++ js/arrow_functions.js
@@ -0,0 +1,11 @@
+function add(a, b){
+    return a + b
+}
+add (2, 3)
+const addArrow = (a, b) =>{
+    return a + b
+}
+addArrow(2, 3)
+
+const addArrowDos = (a, b) => a + b
+console.log(addArrowDos(2, 3))

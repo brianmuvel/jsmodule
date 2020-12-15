368a5e5b221cf52abf9bc7eeff13eefdd3aecdb7
Brianmuvel <brianmuvel@gmail.com>
2020-12-14 22:30:55 -0600

    ADD clase 6 Funciones arrow y metodos de arrays

---
+++ js/arrays.js
@@ -0,0 +1,39 @@
+/* const myArray = [1,4,7]
+
+for (let i = 0; i < myArray.length; i++){
+    const table = myArray[i]
+    for (let j = 1; j <= 10; j++){
+        console.log(`${table} x ${j} = ${table*j}`)
+    }
+} */
+/* 
+
+const arraySuma = [1,2,3,4]
+let total = 0
+
+for(let i = 1; i<= arraySuma.length; i++){
+    total += arraySuma[i-1]
+}
+console.log(total)
+ */
+/* 
+const arreglo = [1,2,3,4]
+arreglo.push(5,6,7,8,9,10)
+console.log(arreglo)
+
+var ages = [3, 10, 18, 20];
+const checkAdult = age => age >= 18
+checkAdult(3)
+let resultado = ages.some(checkAdult)
+console.log(resultado)
+ */
+
+/* 
+array.forEach(element =>{
+    element + 1
+}) */
+//metodos para aprender
+//forEach
+//map
+//filter
+//reduce

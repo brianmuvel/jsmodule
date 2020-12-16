5c50ab20f708cf6f354a6a028fc5c885e9e87206
Brianmuvel <brianmuvel@gmail.com>
2020-12-15 22:27:55 -0600

    ADD clase siete, forEach

--- js/arrays.js
+++ js/arrays.js
@@ -7,7 +7,79 @@ for (let i = 0; i < myArray.length; i++){
     }
 } */
 /* 
+*/
+/* const myArray = [1,4,7]
+myArray.forEach( table =>{
+    for (let i = 1; i <= 10; i++){
+        console.log(`${table} x ${i} = ${table*i}`)
+    }
+})
+*/
 
+//funcion que con pasar aarray separa solo los valores pares
+/*
+/*
+//METODO FOREACH_____________________________________________________-
+*/  
+//const arrNumbers = [1,2,3]
+/* function doubleValue (arrNumbers){
+    arrNumbers.forEach( valor =>{
+        console.log(valor+valor)
+    })
+} */
+const doubleValue = arrNumbers => {
+    let arrayToFill = []
+    arrNumbers.forEach( valor =>{
+        arrayToFill.push(valor*2)
+        //console.log(valor+valor)
+    })
+    return arrayToFill
+}
+let arrDouble = [1,2,3,4]
+const resutadoDoble = doubleValue(arrDouble)
+
+/* 
+function soloPares(array){
+    array.forEach(valor =>{
+        if (valor%2 === 0)
+        console.log(valor)
+    })
+}
+ */
+const soloPares = array => {
+    let vale = []
+    array.forEach(valor =>{
+        if (valor%2 === 0)
+        vale.push(valor)
+        //console.log(valor)
+    })
+    return vale
+}
+const array = [1,2,3,5,6,7]
+const resultadoPares = soloPares(array)
+/*
+function losPrimerosUltimos (stringArray){
+    stringArray.forEach(soloDos =>{
+        let inicial = soloDos.slice(0, 1)
+        let final = soloDos.slice(-1)
+        console.log(inicial+final)
+    })
+}
+ */
+const losPrimerosUltimos = stringArray => {
+    let arrayToFill = []
+    stringArray.forEach(soloDos => {
+        let inicial = soloDos.slice(0, 1)
+        let final = soloDos.slice(-1)
+        arrayToFill.push(inicial+final)
+        //console.log(inicial+final)
+    })
+    return arrayToFill
+}
+const stringArray = ['Hola', 'Mundo']
+let resultadoArray = losPrimerosUltimos(stringArray)
+
+/*
 const arraySuma = [1,2,3,4]
 let total = 0
 
@@ -28,12 +100,16 @@ let resultado = ages.some(checkAdult)
 console.log(resultado)
  */
 
-/* 
-array.forEach(element =>{
-    element + 1
-}) */
-//metodos para aprender
-//forEach
-//map
-//filter
-//reduce
+//METODO MAP________________________________--
+ // .map() 
+ //cuando el resultado que quieres es un ARRAY hay que usar map si no usar for each
+//const newArray = [10,20,3].map(currentValue =>  currentValue*2)
+
+////console.log(newArray)
+
+
+//const doubleValue2 = arrNumbers => arrNumbers.map(valor => valor*2)
+
+const soloPares2 = arrNumber => arrNumber.filter(valor => valor%2===0)
+
+// .reduce() te retorna un acumulador 

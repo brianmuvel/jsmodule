9540b4edeb3a6eb0ee41939a78c36e16cc70fa0e
Brianmuvel <brianmuvel@gmail.com>
2020-12-19 11:12:56 -0600

    ADD clase 8 Objetos, reduce

---
+++ js/objetos.js
@@ -0,0 +1,70 @@
+
+
+let person = {
+    name: 'nayeli',
+    location: {
+        cp: '55778',
+        street:' asadaas'
+    },
+    credential: {
+        key1: '55778',
+        key2:'asadaas'
+    }
+}
+// clonar un objeto
+let person2 = person
+// actualizar una propiedad
+person.location.street = 'reforme'
+// eliminar una propiedad
+delete person.credential.key2
+
+// imprimir cada uno de los valores de la propieda location
+// y credential
+for(item in person) {
+    if(item === 'location' || item === 'credential') {
+        for(item2 in person[item]) {
+            console.log(`${item2}: ${person[item][item2]}`)
+        }
+    } 
+}
+
+// obj koders
+let koders = [
+    {
+        name: 'nayeli',
+        average: 10,
+        location: {
+            cp: '55778',
+            street:' asadaas'
+        }
+    },
+    {
+        name: 'nayeli',
+        average: 9
+    }
+]
+
+// imprimir el promedio de cada koder
+koders.forEach((current, index) => {
+    console.log(current.average)
+})
+
+// imprimir el promedio general de los koders
+let averageKoders = koders.reduce((acc, cv ) => {
+    return acc + cv.average
+}, 0) / koders.length
+console.log(averageKoders)
+
+// Filtrar los koders que tienen promodio mayor a 9
+koders.filter((cv) => {
+    return cv.average > 9
+})
+
+// Filtrar los koders que tengan la propiedad location
+koders.filter((cv) => {
+    if(cv.location) {
+        return cv
+    }
+})
+
+
--- js/tarea.js
+++ js/tarea.js
@@ -1,5 +1,5 @@
 // Ejercicio 1
-// dado el siguiente array de objetos
+// dado el siguiene array de objetos
 let users = [
     { firstName: 'Bradley', lastName: 'Bouley', role: 'Full Stack Resident' },
     { firstName: 'Chloe', lastName: 'Alnaji', role: 'Full Stack Resident' },
@@ -8,36 +8,35 @@ let users = [
     { firstName: 'Robert', lastName: 'Hajek', role: 'Full Stack Resident' },
     { firstName: 'Wes', lastName: 'Reid', role: 'Instructor'},
     { firstName: 'Zach', lastName: 'Klabunde', role: 'Instructor'}
-]
-// obtener un nuevo objeto con esta estructura
-//-> {
-//   'Bradley Bouley': 'Full Stack Resident',
-//   'Chloe Alnaji': 'Full Stack Resident',
-//   'Jonathan Baughn': 'Enterprise Instructor',
-//   'Michael Herman': 'Lead Instructor',
-//   'Robert Hajek': 'Full Stack Resident',
-//   'Wes Reid': 'Instructor',
-//   'Zach Klabunde': 'Instructor'
-//   }
-/* let full = users.forEach(value =>{
-    let fullName = value['firstName']+ ' ' + value['lastName']
-    let role = value['role']
-    console.log( fullName +': '+ role)
-}) */
+];
 
-let joinUsers = users.reduce( (acc, user) => {
+// obtener un nuevo array con esta estructura
+//-> {
+//    'Bradley Bouley': 'Full Stack Resident',
+//   'Chloe Alnaji': 'Full Stack Resident'
+//      ....
+//   }
+
+const joinUsers = users.reduce( (acc, user) => {
     let userName = user.firstName + ' ' + user.lastName
     acc[userName] = user.role
     return acc
 }, {})
-console.log(joinUsers)
 
-//ejercicio 2
+
+
+
+//  tomando el array users,
+//  hacer una funcion que reciba 2 parametros (arrayUsers, role)
+//  y retorne un array con los usuarios que cumplan con el role
 const filterUserByRole = (arr, role) => arr.filter(user => user.role === role )
-let resultado2 = filterUserByRole(users, 'Instructor')
-console.log(resultado2)
+
+
 // ejercicio 3
 // tomando el siguiente array de objetos
+// filtrar lo siguiente
+// 1. Cantidad de personas que votaron
+// 2. Cual es el promedio de Edad de los votantes
 let persons = [
     {name:'Bob' , age: 30, voted: true},
     {name:'Jake' , age: 32, voted: true},
@@ -52,11 +51,9 @@ let persons = [
     {name: 'Jeff', age: 30, voted: true},
     {name: 'Zack', age: 19, voted: false}
 ]
-// obtener lo siguiente
-// 1. Cantidad de personas que votaron
-// 2. Cual es el promedio de Edad de los votantes
+
 const personVoted = persons.reduce( (acc, person) => {
-    return person.voted === true ? acc + 1: acc
+    return person.voted == true ? acc + 1: acc
 }, 0);
 console.log(personVoted)
 
